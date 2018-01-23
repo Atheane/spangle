947f2c0a799181599e43f1b4c2783323559c6a08
Elodie Royant <eroyant@gmail.com>
2018-01-23 23:49:17 +0100

    ecriture des fonctions constructeurs

--- js/background.js
+++ js/background.js
@@ -1,49 +1,113 @@
 
-  var bg, ctxBg;
-  var totalSeconds = 0;
+//////////////////////////////////////////////////////////////////////////
+//                         Gestion des Images                          //
+/////////////////////////////////////////////////////////////////////////
 
-  var background = {
-    src: './assets/backgrounds/background_02_parallax_01.png',
-    init: function() {
-            var img = new Image();
-            img.src = this.src;
-            return img;
-          },
-    draw: function(delay) {
 
-            var img = this.init();
+// objet litéral servant à stocker toutes les images <img>
+// pour ne pas avoir à la regénérer à chaque fois qu'on en a besoin
 
-            img.addEventListener('load', function() {
-              totalSeconds += delay;
+var images = {
+  background: new Image()
+};
 
-              var vx = 1;
-              var vy = 100; // the background scrolls with a speed of 100 pixels/sec
+// pour charger l'image en mémoire
+images.background.src = './assets/backgrounds/background_02_parallax_01.png'
 
-              var numImagesX = Math.ceil(bg.width / img.width) + 1;
-              var numImagesY = Math.ceil(bg.height / img.height) + 1;
 
-              var xpos =  (totalSeconds * vx + spaceShip.x*0.4)  % img.width ;
-              var ypos = (canvas.height - totalSeconds * vy) % img.height;
+// Prototype de tous les futur dessins
+// TO-DO : fonction usine pour minimiser l'espace mémoire
+var Dessin = function() {
+    this.init = function(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+    this.speed = 0;
+    this.canvasWidth = 0;
+    this.canvasHeight = 0;
+}
 
-              ctxBg.save();
-              ctxBg.translate(-xpos, -ypos);
+Dessin.prototype.draw = function() {
+};
 
-              for (var i = 0; i < numImagesX; i++) {
-                for (var j = 0; j < numImagesY; j++) {
-                  ctxBg.drawImage(img, i * img.width, -j * img.height - spaceShip.y*0.4);
-                 }
-               }
-              ctxBg.restore();
-            });
-          }
+var dessin = new Dessin;
+
+
+// Fonction constructeur background
+var Background = function() {
+  this.speed = 1;
+}
+
+Background.prototype.draw = function() {
+  // décale le background en fonction de la vitesse indiquée
+  this.y += this.speed;
+  // la propriété context est définie dans la fonction constructeur Game
+  this.context.drawImage(images.background, this.x, this.y);
+  this.context.drawImage(images.background, this.x, this.y - this.canvasHeigh);
+
+  if (this.y >= this.canvasHeight) {
+    this.y = 0;
   }
+};
+
+// Hérite des propriétés de la fonction constructeur dessin
+Background.prototype = dessin;
 
 
-  document.addEventListener('DOMContentLoaded', function() {
+//////////////////////////////////////////////////////////////////////////
+///                    Fonction Constructeur jeu                       ///
+//////////////////////////////////////////////////////////////////////////
 
-    bg = document.getElementById("background");
-    ctxBg = bg.getContext('2d');
-    bg.width  = window.innerWidth;
-    bg.height = window.innerHeight;
+var Game = function() {
+  this.init = function() {
+    this.bg = document.getElementById('background');
+      // vérifier si canvas est bien supporté par le navigateur
+    if (this.bg.getContext) {
+      this.ctxBg = this.bg.getContext('2d');
+      this.bg.width  = window.innerWidth;
+      this.bg.height = window.innerHeight;
+      // on ajoute les propriétés manquantes à Background
+      // utilisées dans la méthode draw
+      Background.prototype.context = this.ctxBg;
+      Background.prototype.canvasWidth = this.bg.width;
+      Background.prototype.canvasHeight = this.bg.height;
 
-  });
+      this.background = new Background();
+      this.background.init(0,0); // Set draw point to 0,0
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+  // Start the animation loop
+  this.start = function() {
+    animate();
+  };
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+///                         Animation Loop                               ///
+////////////////////////////////////////////////////////////////////////////
+
+var game = new Game();
+
+var animate = function() {
+  requestAnimFrame( animate );
+  game.background.draw();
+}
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame   ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame    ||
+      window.oRequestAnimationFrame      ||
+      window.msRequestAnimationFrame     ||
+      function(callback, element){
+        window.setTimeout(callback, 1000 / 60);
+      };
+})();
+
+
+
