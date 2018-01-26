6a25f74e007774d2a724e2603cf43151246ecf2c
Elodie Royant <eroyant@gmail.com>
2018-01-26 15:12:56 +0100

    bullet, the very beginning

---
+++ scripts/helper/bullet.js
@@ -0,0 +1,10 @@
+
+define(['./image'], function(image) {
+
+  'use strict';
+
+  var Bullet = function() {
+
+  }
+
+});
--- scripts/helper/image.js
+++ scripts/helper/image.js
@@ -1,18 +1,21 @@
-define(function () {
+define(['../jquery'], function ($) {
 
   'use strict';
 
   var image = {};
   image.background = new Image;
   image.player = new Image;
+  // image.bullet = new Image;
 
   var imageLoading = function() {
-    image.player.addEventListener('load', function() {
-      console.log('Player Image Loaded');
-    });
-    image.background.addEventListener('load', function() {
+    $(image.background).on('load', function() {
       console.log('Background Image Loaded');
     });
+    $(image.player).on('load', function() {
+      console.log('Player Image Loaded');
+    });
+    // image.bullet.
+
     image.background.src = '../../assets/backgrounds/background_02_parallax_01.png';
     image.player.src = '../../assets/spaceships/EnemyDefense.png';
   };
