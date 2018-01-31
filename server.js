14ece1deefc7e9a9042d47d2a781397b5fe46c5f
Elodie Royant <eroyant@gmail.com>
2018-02-01 00:30:14 +0100

    setup for publishing

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const port = process.env.PORT;
-const port = 3000;
+const port = process.env.PORT;
+// const port = 3000;
 
 
 app.use(express.static('public'));
