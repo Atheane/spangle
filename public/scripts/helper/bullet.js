67de0e4e3ef128a17c26f26608d71d9ef62c7fed
Elodie Royant <eroyant@gmail.com>
2018-02-28 23:46:52 +0100

    hot fix to have less dirty rectangles

--- public/scripts/helper/bullet.js
+++ public/scripts/helper/bullet.js
@@ -15,8 +15,8 @@ define(['./image', './dessin'], function(image, Dessin) {
   Bullet.prototype.draw = function() {
     // this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
 
-    this.context.clearRect(this.x, this.y, this.width, this.height);
-    this.context.clearRect(this.x+44, this.y, this.width, this.height);
+    this.context.clearRect(this.x-20, this.y-20, this.width+40, this.height+40);
+    this.context.clearRect(this.x+14, this.y-20, this.width+40, this.height+40);
     this.y -= this.speed;
     if (this.y > 0 - this.height) {
       this.context.drawImage(image.bulletShort, this.x, this.y, this.width, this.height);
